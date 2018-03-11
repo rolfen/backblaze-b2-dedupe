4557eea706f985f6c2ac20ed94ca263fc3fa1b8a
Rolf <r10@eebe.be>
2018-03-11 20:27:22 +0200

    WIP:

--- dupes.js
+++ dupes.js
@@ -1,19 +1,54 @@
-const fs = require('fs');
+const readline = require('readline');
+const minimist = require("minimist");
+
+var hash = {};
 
 // parse arguments
-const minimist = require("minimist");
 var args = minimist(process.argv.slice(2),{
 	alias: {
+		/* put flags here
 		i: "inverse",
 		l: "local",
 		r: "remote"
+		*/
 	}
 });
 
-debugger;
+var lineRead = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  terminal: false
+});
 
-function arrayFromFile(f) {
-	return fs.readFileSync(f).toString().split("\n").map((line)=>{
-		return line.split(' ');
-	}).filter((el)=>{el !== undefined});
-} 
+
+lineRead.on('line', function (line) {
+	var sha, path;
+	[sha, path] = line.split('/ +/', 2);
+	if(sha in hash) {
+		// exists
+		hash[sha].push(path);
+	} else {
+		// new
+		hash[sha] = [path];
+	}
+});
+
+lineRead.on('close', function (line) {
+	debugger;
+	for(sha in hash) {
+		if(hash.hasOwnProperty(sha)) {
+			var files = hash[sha];
+			switch(process.argv[2]) {
+				case "count":
+				break;
+				default:
+					if(files.length > 1) {
+						process.stdout.write(
+							$sha + "\n" + files.join("\n") + "\n"
+						);
+					}
+				break;
+			}			
+		}
+	}
+});
--- dupes.pl
+++ dupes.pl
@@ -1,33 +1,45 @@
 #!/usr/bin/perl
+my %hash; 
 
-my %hash; # Key: checksum. Values: (reference to) array of file paths
 
-while($line = <STDIN>) {
-	my($sha, $path) = split(/ +/, $line, 2);
-	if(exists $hash{$sha}) {
-		# duplicate
-		push(@{$hash{$sha}}, $path);
+sub myPush {
+	# $hash is a reference to hash of arrays
+	# every entry in %hash should be an array
+	# $hashRef is the key
+	my ($hashRef, $value) = @_;
+	if(exists $hash{$hashRef}) {
+		push(@{$hash{$hashRef}}, $value);
 	} else {
-		$hash{$sha} = [($path)];
+		# create new array for key $hashRef
+		$hash{$hashRef} = [($value)];
 	}
 }
 
+# Key: checksum. Values: (reference to) array of file paths
 
-foreach $key (keys %hash) {
-	if($ARGV[0] eq "count") {
+while($line = <STDIN>) {
+	my($sha, $path) = split(/ +/, $line, 2);
+	myPush($sha, $path, \%hash);
+}
+if($ARGV[0] eq "count") {
+	# print counts for each hash
+	foreach $key (keys %hash) {
 		print $key." ".scalar(@{$hash{$key}})."\n";
-	} elsif($ARGV[0] eq "dirs") {
-		use File::Basename;
-		my %dir;
-		while(my($k, $v) = each(%hash)) {
-			my $dirpath = basename($v);
-			if(scalar(@{$v}) > 1) {
-				# duplicates
-			} elsif(scalar(@{$v}) == 1) {
-				# single file
-			}
-		}
-	} else {
+	}
+#} elsif($ARGV[0] eq "dirs") {
+#	my %dirs;
+#	use File::Basename;
+#	while(my($k, $v) = each(%hash)) {
+#		# my $dirpath = basename($v);
+#		if(scalar(@{$v}) > 1) {
+#			# duplicates
+#		} elsif(scalar(@{$v}) == 1) {
+#			# single file
+#		}
+#	}
+} else {
+	# print groups of duplicates
+	foreach $key (keys %hash) {
 		if(scalar(@{$hash{$key}}) > 1) {
 			print $key."\n".join("", @{$hash{$key}})."\n";
 		}			
