195ac7d8b2def8c6931e9b050bed101bcbe22f18
Rolf <r10@eebe.be>
2018-03-21 10:22:27 +0200

    tidy-up

--- modules/dupes/index.js
+++ modules/dupes/index.js
@@ -2,11 +2,7 @@
 'use strict';
 
 
-/* TODO:
- Mutliple linked lists
- Maybe:
-
- FileList, HashList, DirList
+/* 
 
  File {
  	filename: string,
@@ -24,15 +20,14 @@
 	files: {File indexed list}
  }
 
-DirectoryList {
+FileCollection, HashCollection, DirCollection:
+
+DirectoryCollection {
 	items: {Directory indexed list}
 }
 
-Functions as methods
-Eg: 
-DirectoryList.getCombinations()
-HashList.merge(someHashItem)
-.unique() sounds better than .merge()
+etc.
+
 */
 
 function connect(item1, item2) {
@@ -62,6 +57,10 @@ class Item {
 
 Item.prototype.setOnce = setPropertyOnce;
 
+
+// +++++++++++++++++++++++++++++++++
+
+
 class File extends Item {
 
 	constructor (value) {
@@ -81,7 +80,6 @@ class File extends Item {
 
 class FileCollection extends Collection {}
 
-
 class Hash extends Item {
 
 	constructor (value) {
@@ -98,9 +96,7 @@ class Hash extends Item {
 	}
 }
 
-
 class HashCollection extends Collection {}
-
 
 class Directory extends Item {
 
@@ -117,38 +113,16 @@ class Directory extends Item {
 		}
 	}
 }
+
 class DirectoryCollection extends Collection {}
 
 
-// must check code below
-DirectoryCollection.prototype.fromHashList = function(hashList) {
-	const path = require('path');
-	for(var sha1 in hash) {
-		if(hashList.hasOwnProperty(sha1)) {
+// =============
 
-			hash[sha].forEach(function(file){
-				var dirEntry = new DirItem();
-				var dirname = path.dirname(file);
-				var basename = path.basename(file);
-				if(!(dirname in dir)) {
-					dir[dirname] = [[],[]];
-				}
-				dirEntry = dir[dirname];
-				if(hash[sha].length > 1) {
-					dirEntry[DUPLICATE].push(basename);
-				} else if (hash[sha].length == 0){
-					dirEntry[UNIQUE].push(basename);							
-				}
-				dir[dirname] = dirEntry;
-			});
-		}
-	}
-	return this;
-}
 
 module.exports = {
-	Item,
-	Collection,
+	Item, // export for testing
+	Collection, // export for testing
 	File,
 	FileCollection,
 	Hash,
@@ -158,175 +132,3 @@ module.exports = {
 	connect
 };
 
-// =============
-
-// Code below is for reference
-
-/*
- * @param {HashList} hash
- * @return {DirList} 
- */
-
-const DUPLICATE = 0;
-const UNIQUE = 1;
-
-const hash = {};
-
-function dirList(hash) {
-	const path = require('path');
-	var dir = {};
-	for(var sha in hash) {
-		if(hash.hasOwnProperty(sha)) {
-			hash[sha].forEach(function(file){
-				var dirEntry;
-				var dirname = path.dirname(file);
-				var basename = path.basename(file);
-				if(!(dirname in dir)) {
-					dir[dirname] = [[],[]];
-				}
-				dirEntry = dir[dirname];
-				if(hash[sha].length > 1) {
-					dirEntry[DUPLICATE].push(basename);
-				} else if (hash[sha].length == 0){
-					dirEntry[UNIQUE].push(basename);							
-				}
-				dir[dirname] = dirEntry;
-			});
-		}
-	}
-	return dir;
-}
-
-function mdAdd(key, val, hash) {
-	if(!(key in hash)) {
-		hash[key] = [];
-	} 
-	hash[key].push(val);
-}
-
-
-function skipThis() {
-	return false;
-
-	lineRead.on('line', function (line) {
-		var sha = line.substr(0, 40);
-		var path = line.substr(41);
-		mdAdd(sha, path, hash);
-	});
-
-
-	lineRead.on('close', function (line) {
-		switch(process.argv[2]) {
-			case "count":
-			break;
-			case "affinity":
-				/*
-					
-
-					Alternative: 
-					Compile a list of UniqueFile objects
-					We sort of already have this in the gloabl hash object
-					But I guess it would be nice to have the sha1 inside the object so that we can move it around without loosing this information.
-					We can then also add properties / methods.
-					(but do we need to?)
-
-					
-					uniqueFile {
-						sha1 89278ca987e9ae772d01f9278ca987de8a717e9a
-						paths [
-							/a/b/file.orf
-							/x/y/sameFile.orf
-						]
-					}
-
-					Then group them in directory sets
-					Then it will be fast to find intersection by comparing object references
-					Maybe we don't need set-manipulator
-
-					Alternative:
-					Keep it as it is now, and add directory set arrays of sha1 string,
-					and compare using the set-manipulator module
-
-					In any case would be nice to use "classes" to abstract and decouple these details from the code
-				
-				*/
-
-				// todo: unit testing
-				
-				/*
-				const affinity = {};
-				const directories = {};
-				const intersections = {};
-				function Set() {
-					this.intersections = {};
-				}
-				function Intersection() {
-					this.set0 = undefined; // holds reference to set0
-					this.set1 = undefined; // as above
-					this.points = [];
-				}
-				function SetList() {
-					this.set = {};
-				} 
-				SetList.prototype.addIntersection = function(set0, set1, point) {
-					if(!this.sets[set0]) {
-
-					}
-				}
-				DirectoryList.prototype.
-				function addIntersection(dir0, dir1, path) {
-					var intersection = {
-						'path' : path
-					};
-					directories[dir0] = intersection;
-					directories[dir1] = intersection;
-				}
-				*/
-				var so = require('setops');
-				
-				var dir = dirList(hash);
-				var combinations = [];
-				var paths = Object.keys(dir);
-				// find all combinations of hashes
-				paths.forEach(function(hashA, i){
-					paths.slice(i+1).forEach(function(hashB){
-						combinations.push([hashA, hashB]);
-						console.log('[' + hashA + ', ' + hashB + ']' );
-					});
-				});
-			break;
-			case "dirlist":
-				var dir = dirList(hash)
-				for(var dirname in dir) {
-					if(dir.hasOwnProperty(dirname)) { 
-						process.stdout.write(dirname + "\n");
-					}
-				}
-			break;
-			case "dirs":
-				var dir = dirList(hash)
-				for(var dirname in dir) {
-					if(dir.hasOwnProperty(dirname) && (dir[dirname][DUPLICATE].length > 0)) { 
-						process.stdout.write(dirname + "\n");
-						process.stdout.write("Dupe: " + dir[dirname][DUPLICATE].length + "\n");
-						process.stdout.write("Uniq: " + dir[dirname][UNIQUE].length + "\n");
-						process.stdout.write("\n");
-					}
-				}
-			break;
-			default:
-				for(sha in hash) {
-					if(hash.hasOwnProperty(sha)) {
-						var files = hash[sha];
-						if(files.length > 1) {
-							process.stdout.write(
-								sha + "\n" + files.join("\n") + "\n\n"
-							);
-						}
-					}
-				}
-			break;
-		}
-	});
-
-}
--- modules/dupes/test.js
+++ modules/dupes/test.js
@@ -26,29 +26,21 @@ const connect = require('./index.js').connect;
 
 
 !function() {
-
 	console.log("Item");
 	var item = new Item(bogusFilename);
 	assert.strictEqual(item.value, bogusFilename);
-
 	const propName = 'foo';
 	const propVal = 'baz';
-
 	console.log("setOnce");
-	
 	item.setOnce(propName,propVal);
 	assert.strictEqual(item[propName], propVal);
-
 	assert.throws(()=>{
 		item.setOnce(propName,propVal);
 	});
-
 }();
 
 !function() {
-
 	console.log("File");
-
 	var file = new File(bogusFilename);
 	assert.strictEqual(file.value, bogusFilename);
 
@@ -72,93 +64,24 @@ const connect = require('./index.js').connect;
 
 !function() {
 	console.log("Directory");
-
 	var dir = new Directory(bogusDirname);
 	assert.strictEqual(dir.value, bogusDirname);
-
 	console.log("Directory: linked objects");
-
 	var file = new File();
-
 	dir.files[bogusFilename] = file;
-
 	var tmpObj = {};
 	tmpObj[bogusFilename] = file;
-
 	assert.deepEqual(dir.files, tmpObj);
-
-
 }();
 
-
 !function() {
-
 	console.log("connect(file, dir)");
-
 	var file = new File();
 	file.value = bogusFilename;
-
 	var dir = new Directory();
 	dir.value = bogusDirname;
-
 	connect(dir, file);
-
 	assert.strictEqual(file.directory, dir);
 	assert.strictEqual(dir.files[bogusFilename], file);
-
-
 }()
 
-/* meh
-
-!function() {
-	console.log("DirItem Object");
-
-	// ===============
-
-	var dirItem = new DirItem();
-	dirItem.path = "/my/dir";
-
-	fileItem1 = new FileItem();
-	fileItem1.filename = "one.png";
-	fileItem1.hash = new HashItem(); //
-	fileItem1.hash.sha1 = "8f627205dfae0cbe639149db8a2acf5410cc37f6"
-	fileItem1.hash.files = new FileList(); //
-	fileItem1.hash.files.add(fileItem1); //
-	fileItem1.directory = new DirectoryItem(); //
-	fileItem1.directory.path = "/some/dir";
-	fileItem1.directory.files = new FileList(); //
-	fileItem1.directory.files.add(fileItem1); //
-
-	dirItem.files.add(fileItem1);
-
-	// ================
-
-	var hashList = new HashList();
-	var directoryList = new DirectoryList();
-	var fileList = new FileList();
-
-	fileItem = directoryList.merge({
-		filename: "one.png",
-		dirname: "/some/dir"
-	});
-
-	fileList.merge({
-		filename: "one.png",
-		directory: [fileItem.directory],
-		hash: [fileItem.hash]
-	});
-
-
-	fileItem.hash = hashList.merge({
-		sha1: "8f627205dfae0cbe639149db8a2acf5410cc37f6",
-		files: [fileItem]
-	});
-
-	fileItem.directory = directoryList.merge({
-		directory: "/some/dir",
-		files: [fileItem]
-	});
-}
-
-*/
